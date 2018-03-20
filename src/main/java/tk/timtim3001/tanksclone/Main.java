4d8e0fb50104ab0d5247259cd9597eaa2af7a786
Unknown <timtim3001@gmail.com>
2018-03-21 00:24:05 +0100

    Add the correct look and feel

--- src/main/java/tk/timtim3001/tanksclone/Main.java
+++ src/main/java/tk/timtim3001/tanksclone/Main.java
@@ -1,8 +1,16 @@
 package tk.timtim3001.tanksclone;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String args[]){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         new Frame();
     }
 }
