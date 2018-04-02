c4613e371b31cec8435dec6c5ca62d0983de9994
Unknown <timtim3001@gmail.com>
2018-04-02 23:54:18 +0200

    Auto stash before merge of "develop" and "origin/develop"

---
+++ engine/src/main/java/tk/timtim3001/engine/input/MouseKeys.java
@@ -0,0 +1,10 @@
+package tk.timtim3001.engine.input;
+
+public class MouseKeys {
+
+    public static final int MOUSE_KEY_0 = 0;
+    public static final int MOUSE_KEY_1 = 1;
+    public static final int MOUSE_KEY_2 = 2;
+
+
+}
